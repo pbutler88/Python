635d8462a4f0a3a8ce9f761e0a1dd043ac56f47e
pbutler88 <pbutler88@yahoo.com>
2019-09-28 20:54:28 -0700

    Adding simple file

---
+++ BeerApp.py
@@ -0,0 +1,20 @@
+#----------------------------
+#BEER APP
+#----------------------------
+def greet_user():
+ """Display a simple greeting."""
+ print('Hi,' + FirstName + ' ' + LastName + ' thanks for joining us inside the beer app!')
+
+print ('-----------------------------------------')
+print('- Welcome to the beer app')
+print ('-----------------------------------------')
+
+FirstName = input("Hi, What's your first name? ")
+LastName = input("Hi, What's your last name? ")
+
+greet_user()
+print('')
+
+print('What kind of beer do you like?')
+print('')
+
